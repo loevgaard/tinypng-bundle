50c00aceb16b6ecc2cde896bd5144b599c2141e7
loevgaard <joachim@loevgaard.dk>
2016-09-06 11:15:41 +0200

    Removed services because it wasn't loaded automatically of course ._. Runs the static method in the TinypngExtension instead.

--- DependencyInjection/TinypngExtension.php
+++ DependencyInjection/TinypngExtension.php
@@ -2,7 +2,6 @@
 namespace Tinypng\Bundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
@@ -13,9 +12,6 @@ class TinypngExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $container->setParameter('tinypng.api_key', $config['api_key']);
-
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('services.yml');
+        \Tinify\setKey($config['api_key']);
     }
 }
